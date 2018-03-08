33dc0a82f3c4890a6c427cc245fab26db294f7d4
Pankaj Kulkarni <pankajvkulkarni@icloud.com>
2018-03-08 14:12:08 +0530

    Reorganised as MVC pattern by introducing a new model class Item and used it to store hold item title and done status. But the problem is now the App crashed when a new item is added as UserDefaults does not allow storage of array of items.

--- MyTodo/Controller/ToDoListViewController.swift
+++ MyTodo/Controller/ToDoListViewController.swift
@@ -11,14 +11,27 @@ import UIKit
 class ToDoListViewController: UITableViewController {
 
     
-    var itemArray = ["Find Mike", "Buy Eggs", "Destroy Yourself"]
+    var itemArray = [Item]() //["Find Mike", "Buy Eggs", "Destroy Yourself"]
     let defaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        if let items = defaults.array(forKey: "ToDoListArray") as? [String] {
+        let newItem = Item()
+        newItem.title = "Find Mike"
+        itemArray.append(newItem)
+        
+        let newItem2 = Item()
+        newItem2.title = "Buy Eggs"
+        itemArray.append(newItem2)
+        
+        let newItem3 = Item()
+        newItem3.title = "Destroy Yourself"
+        itemArray.append(newItem3)
+
+        
+        if let items = defaults.array(forKey: "ToDoListArray") as? [Item] {
             itemArray = items
         }
         
@@ -35,9 +48,15 @@ class ToDoListViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
-        cell.textLabel?.text = itemArray[indexPath.row]
         
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        let item = itemArray[indexPath.row]
+        
+        cell.textLabel?.text = item.title
+        
+        cell.accessoryType = item.done ? .checkmark : .none
+  
         return cell
     }
     
@@ -45,11 +64,10 @@ class ToDoListViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        } else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+        
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        
+        tableView.reloadData()
         
         tableView.deselectRow(at: indexPath, animated: true)
     }
@@ -67,7 +85,9 @@ class ToDoListViewController: UITableViewController {
             // Once user clicks Add Item Button on the alert
             print("Add Item pressed with new ietm: \(textField.text!)")
             
-            self.itemArray.append(textField.text!)
+            let newItem = Item()
+            newItem.title = textField.text!
+            self.itemArray.append(newItem)
             
             self.defaults.setValue(self.itemArray, forKey: "ToDoListArray")
             
---
+++ MyTodo/Data Model/Item.swift
@@ -0,0 +1,17 @@
+//
+//  Item.swift
+//  MyTodo
+//
+//  Created by Pankaj Kulkarni on 08/03/18.
+//  Copyright © 2018 Pankaj Kulkarni. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title = ""
+    var done = false
+    
+    
+}
